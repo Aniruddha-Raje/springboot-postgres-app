12e169267f03ce32537f237863b589acc4765d02
Aniruddha Raje <aniruddharaje@Aniruddhas-MacBook-Air.local>
2024-10-20 12:37:53 +0530

    added aop advise

---
+++ src/main/java/com/example/springboot_postgres/helpers/AspectUtils.java
@@ -0,0 +1,24 @@
+package com.example.springboot_postgres.helpers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class AspectUtils {
+
+
+//    @Around("@annotation(LogExecutionTime)")
+    @Around("execution(* com.example.springboot_postgres.service.AppService.getAllAppUsers(..)))")
+    public Object executionTime(ProceedingJoinPoint point) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object object = point.proceed();
+        long endtime = System.currentTimeMillis();
+        log.info("Class Name: "+ point.getSignature().getDeclaringTypeName() +". Method Name: "+ point.getSignature().getName() + ". Time taken for Execution is : " + (endtime-startTime) +"ms");
+        return object;
+    }
+}
--- src/main/java/com/example/springboot_postgres/service/AppService.java
+++ src/main/java/com/example/springboot_postgres/service/AppService.java
@@ -2,25 +2,29 @@ package com.example.springboot_postgres.service;
 
 import com.example.springboot_postgres.model.AppUser;
 import com.example.springboot_postgres.repository.AppUserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class AppService {
 
     @Autowired
     AppUserRepository userRepository;
 
+    @LogExecutionTime
     public List<AppUser> getAllAppUsers(){
-
+        log.info("inside getAllAppUsers");
         return userRepository.findAll();
     }
 
+    @LogExecutionTime
     public Optional<AppUser> getUser(Long id){
-
+        log.info("inside getUser");
         return userRepository.findById(id);
     }
 
